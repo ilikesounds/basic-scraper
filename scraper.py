4eb44dbfdc0e822f83f44047de863e05e19a96b6
ilikesounds <email_id>
2016-09-11 12:52:27 -0700

    added file write function

---
+++ scraper.py
@@ -0,0 +1,41 @@
+import requests
+
+
+INSPECTION_DOMAIN = 'http://info.kingcounty.gov'
+INSPECTION_PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
+INSPECTION_PARAMS = {
+    'Output': 'W',
+    'Business_Name': "Ivar's Salmon House",
+    'Business_Address': '401 NE Northlake Way',
+    'Longitude': '',
+    'Latitude': '',
+    'City': 'Seattle',
+    'Zip_Code': '98105',
+    'Inspection_Type': 'All',
+    'Inspection_Start': '',
+    'Inspection_End': '',
+    'Inspection_Closed_Business': 'A',
+    'Violation_Points': '',
+    'Violation_Red_Points': '',
+    'Violation_Descr': '',
+    'Fuzzy_Search': 'N',
+    'Sort': 'B'
+}
+
+
+def get_inspection_page(**kwargs):
+    url = INSPECTION_DOMAIN + INSPECTION_PATH
+    params = INSPECTION_PARAMS.copy()
+    for key, val in kwargs.items():
+        if key in INSPECTION_PARAMS:
+            params[key] = val
+    resp = requests.get(url, params=params)
+    write_to_file(resp.content)
+    resp.raise_for_status() # <- This is a no-op if there is no HTTP error
+    # remember, in requests `content` is bytes and `text` is unicode
+    return resp.content, resp.encoding
+
+def write_to_file(results):
+    file = open('results.html', 'w')
+    file.write(str(results))
+    file.close()
